cd3970882c91bd780f125f3ed2fd893e292617c3
changhy2001 <changhy2001@naver.com>
2024-11-14 19:12:29 +0900

    Feat: 영업/정비 시간 UI 및 로직 구현 영업/정비 시간 UI 및 5분 타이머 종료에 따른 정비시간 전환 로직 구현 * 게임 시작 버튼 누르면 영업 시간 5분 타이머 작동 시작 * 5분이 지나면 정비 시간으로 자동 전환 * 정비 시간에 영업 시작 버튼 누르면 다시 영업 시간으로 전환 * 그 외 ‘정비 시간에만 인테리어 수정 가능하도록 하는 기능’과 ‘영업 시간의 손님 로직’과의 연결은 추가할 예정 * + UI 이미지 파일 하나 import 해왔습니다. Resolves: SCRUM-195

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    private Timer timer;
+    public Button startGame; //게임 시작 버튼 (테스트용)
+    public Button openRestaurantButton;
+
+    [SerializeField] private TMP_Text openOrCloseText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = GameObject.Find("Timer").GetComponent<Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        OpenRestaurant();
+    }
+
+    //영업 시간 시작
+    public void OpenRestaurant()
+    {
+        timer.StartTimer();
+        openOrCloseText.text = "영업 시간";
+        //영업 시작 버튼 비활성화
+        openRestaurantButton.gameObject.SetActive(false);
+        //손님 prefab 들어오기 시작
+    }
+
+    //정비 시간 시작 
+    public void CloseRestaurant()
+    {
+        openOrCloseText.text = "정비 시간";
+        //영업 시작 버튼 생성
+        openRestaurantButton.gameObject.SetActive(true);
+        //손님 prefab 멈추기
+        //인테리어 수정 가능 모드로 전환 
     }
 }
