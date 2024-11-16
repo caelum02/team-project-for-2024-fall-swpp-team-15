508921abf203b3c3670bf25bfac4e4afa05d59bc
Junhee Kim <rlawnsgml0919@gmail.com>
2024-11-16 11:40:38 +0900

    Feat: Interact Distance 추가
    PlayerController에서 Raycast로 Interact를 구현했습니다.
    중간에 하다가 branch 새로 만드는 것을 까먹어서 다른 브랜치 파서 다시 시작할 예정입니다.
    
    Resolves: SCRUM-153

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+        if(Input.GetKey(KeyCode.W)){
+            inputVector.y += 1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            inputVector.y -= 1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            inputVector.x -= 1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance)){
+            Debug.Log(raycastHit.transform);
+        }
+    }
+
+    private void HandleMovement(){
         Vector2 inputVector = new Vector2(0,0);
         if(Input.GetKey(KeyCode.W)){
             inputVector.y += 1;
