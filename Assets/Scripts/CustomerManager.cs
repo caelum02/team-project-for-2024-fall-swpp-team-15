8fa0e18bca9ad342c3757bfea04a370dd9310078
changhy2001 <changhy2001@naver.com>
2024-11-25 15:11:56 +0900

    Feat: 음식(Food) 데이터베이스 구현 및 주석 작업
    - 음식(재료, 중간단계, 요리) 데이터베이스를 관리하는 Scriptable Object
    - 음식 이름, 태그, 가격, 레벨, 프리팹 등을 저장
    
    Resolves: SCRUM-211

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -5,27 +5,56 @@ using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 손님 로직을 관리하는 클래스.
+/// 손님 생성, 착석, 퇴장 관리. 
+/// </summary>
 public class CustomerManager : MonoBehaviour
 {
+    /// <summary>
+    /// 손님 경로 탐색을 위한 NavMeshSurface
+    /// </summary>
     public NavMeshSurface floor;
+
+    /// <summary>
+    /// 레스토랑의 테이블 리스트
+    /// </summary>
     public List<Table> tables = new List<Table>();
+
+    /// <summary>
+    /// 현재 레스토랑에 있는 손님 GameObject 리스트
+    /// </summary>
     public List<GameObject> customers = new List<GameObject>();
+
+    /// <summary>
+    /// 손님 GameObject 프리팹
+    /// </summary>
     public GameObject customerPrefab;
+
+    /// <summary>
+    /// 손님 '주문 관리'를 위한 OrderManager
+    /// </summary>
     public OrderManager orderManager;
 
-    // 손님 생성 Wave 인터페이스
-    // 외부에서 설정 가능하도록 public으로 선언 ex) 평판 변경으로 난이도 상승 시 손님 생성 Wave 변경
+    /// <summary>
+    /// 손님 생성 Wave 인터페이스.
+    /// 외부에서 설정 가능하도록 public으로 선언 ex) 평판 변경으로 난이도 상승 시 손님 생성 Wave 변경
+    /// </summary>
     public ICustomerWave customerWave;
-    // 손님 생성 Coroutine
+
+    /// <summary>
+    /// 손님 생성 Coroutine
+    /// </summary>
     private Coroutine customerSpawnCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
         FindTable();
-
-        // 손님 생성 Wave 설정
-        // 최초에는 4초 후부터 4초 간격으로 손님 생성하는 로직 적용
+        /// <summary>
+        /// 손님 생성 Wave 설정
+        /// 최초에는 4초 후부터 4초 간격으로 손님 생성하는 로직 적용
+        /// </summary>
         customerWave = new UniformCustomerWave(4f, 4f);
     }
 
@@ -35,7 +64,9 @@ public class CustomerManager : MonoBehaviour
         
     }
 
-    //손님 들어오기 
+    /// <summary>
+    /// 손님 입장 시작
+    /// </summary>
     public void StartCustomerEnter()
     {
         Debug.Log("영업 시작");
@@ -45,7 +76,9 @@ public class CustomerManager : MonoBehaviour
         customerSpawnCoroutine = StartCoroutine(customerWave.SpawnCustomers(this));
     }
 
-    //손님 나가기 
+    /// <summary>
+    /// 손님 퇴장 및 레스토랑 초기화
+    /// </summary>
     public void StartCustomerExit()
     {
         // 손님 생성 coroutine 중지
@@ -55,7 +88,9 @@ public class CustomerManager : MonoBehaviour
         FindCustomerExit();
     }
 
-    // 영업 시간 시작 직후 모든 Table 찾기 
+    /// <summary>
+    /// 영업 시간 시작 직후 모든 Table 찾기
+    /// </summary>
     public void FindTable()
     {
         // TODO: NavMesh 초기화하는 코드는 다른 곳으로 옮기기
@@ -72,7 +107,9 @@ public class CustomerManager : MonoBehaviour
         ShuffleTables();
     }
 
-    // 테이블 리스트 랜덤으로 섞기 
+    /// <summary>
+    /// 테이블 리스트 랜덤으로 섞기 
+    /// </summary>
     public void ShuffleTables()
     {
         System.Random random = new System.Random();
@@ -85,7 +122,10 @@ public class CustomerManager : MonoBehaviour
         }
     }
 
-    // 착석 가능한 Table 찾기 
+    /// <summary>
+    /// 사용(착석) 가능한 테이블 반환 
+    /// </summary>
+    /// <returns>사용 가능한 <see cref="Table"/> 또는 없을 경우 null.</returns>
     public Table GetAvailableTable()
     {
         foreach (Table table in tables)
@@ -99,14 +139,20 @@ public class CustomerManager : MonoBehaviour
         return null;
     }
 
-    // 손님 한 명 생성 
+    /// <summary>
+    /// 손님 한 명 생성 
+    /// </summary>
     public void SpawnCustomer()
     {
         GameObject customer = Instantiate(customerPrefab);
         customers.Add(customer);
     }
 
-    // 주문 관리 
+    /// <summary>
+    /// 손님 주문 처리
+    /// </summary>
+    /// <param name="customer">주문하는 손님</param>
+    /// <param name="dishName">주문한 음식 이름</param>
     public void HandleOrder(CustomerNPC customer, string dishName)
     {
         Debug.Log($"Processing Order for {customer.name}: {dishName}");
@@ -117,7 +163,9 @@ public class CustomerManager : MonoBehaviour
         // 음식 조리 시작하기 
     }
 
-    // 손님 나가기 
+    /// <summary>
+    /// 손님 퇴장  
+    /// </summary>
     private void FindCustomerExit()
     {
         foreach (GameObject customer in customers)
@@ -130,21 +178,31 @@ public class CustomerManager : MonoBehaviour
     }
 }
 
-// 손님이 들어오는 로직을 관리하는 인터페이스
+/// <summary>
+/// 손님이 들어오는 로직을 관리하는 인터페이스
+/// </summary>
 // SpawnCustomer: 손님을 생성하는 Coroutine
 public interface ICustomerWave {
-    // 손님 생성 Coroutine
+    /// <summary>
+    /// 손님을 생성하는 코루틴
+    /// </summary>
+    /// <param name="customerManager"><see cref="CustomerManager"/> 인스턴스.</param>
+    /// <returns>코루틴 Eumerator </returns>
     public abstract IEnumerator SpawnCustomers(CustomerManager CustomerManager);
 }
 
-// 일정한 시간 간격으로 손님을 생성하는 클래스
-// startDelay: 손님 생성 시작까지의 대기 시간
-// spawnInterval: 손님 생성 간격
+/// <summary>
+/// 일정한 시간 간격으로 손님을 생성하는 클래스
+/// </summary>
 public class UniformCustomerWave: ICustomerWave {
 
     private float startDelay;
     private float spawnInterval;
-
+    /// <summary>
+    /// <see cref="UniformCustomerWave"/>의 새 인스턴스를 초기화
+    /// </summary>
+    /// <param name="startDelay">손님 생성 시작 전 대기 시간</param>
+    /// <param name="spawnInterval">손님 생성 간격</param>
     public UniformCustomerWave(float startDelay, float spawnInterval) {
         this.startDelay = startDelay;
         this.spawnInterval = spawnInterval;
---
+++ Assets/Scripts/FoodDatabaseSO.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 음식(Food) 데이터베이스(재료, 중간단계, 요리)를 관리하는 ScriptableObject
+/// 음식 정보를 저장하고 에디터에서 관리할 수 있음 
+/// </summary>
+[CreateAssetMenu]
+public class FoodDatabaseSO : ScriptableObject
+{
+    /// <summary>
+    /// 음식 데이터 리스트
+    /// 각각의 항목은 <see cref="FoodData"/> 객체로 구성됨
+    /// </summary>
+    public List<FoodData> foodData;
+}
+
+/// <summary>
+/// 개별 음식 데이터 정보를 저장하는 클래스
+/// </summary>
+[Serializable]
+public class FoodData
+{
+    /// <summary>
+    /// 음식 이름
+    /// </summary>
+    [field: SerializeField]
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// 음식 태크 (재료, 중간단계, 요리)
+    /// </summary>
+    [field: SerializeField]
+    public string tag { get; private set; }
+
+    /// <summary>
+    /// 음식 고유 ID
+    /// </summary>
+    [field: SerializeField]
+    public int ID { get; private set; }
+    
+    /// <summary>
+    /// 음식 가격 
+    /// </summary>
+    [field: SerializeField]
+    public int price { get; private set; }
+
+    /// <summary>
+    /// 음식에 필요한 레벨  
+    /// </summary>
+    [field: SerializeField]
+    public int level { get; private set; }
+    
+    /// <summary>
+    /// 음식 프리팹 
+    /// </summary>
+    [field: SerializeField]
+    public GameObject Prefab { get; private set; }
+}
